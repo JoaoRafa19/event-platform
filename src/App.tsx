97bad2c649538eba5296fb88807e70506208c908
João Pedro Rafael Santos Silva <joaopedrorafael19@gmail.com>
2022-06-20 22:21:48 -0300

    primeira aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+import { gql, useQuery } from "@apollo/client";
+import { useEffect } from "react"
+
+const GETLESSONS = gql`
+  query {
+    lessons{
+      id
+      title
+      teacher {
+        name
+      }
+    }
+  }
+`
+
+interface Lesson {
+  id: string
+  title: string
+  teacher: {
+    name: string
+  }
+}
 
 function App() {
 
+  const {data} = useQuery<{lessons:Lesson[]}>(GETLESSONS);
+  
+
   return (
-  <h1 className="title"> Hello World</h1>
+  <ul>
+    {
+      data?.lessons.map(lesson => (
+        <li key={lesson.id}> {lesson.title} </li> ) )
+    }
+  </ul>
     )
 }
 
